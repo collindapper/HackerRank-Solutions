9f9b23808285360ba3f685ddf4ef17cd0a2e7320
Collin Dapper <collindapper@gmail.com>
2022-12-16 19:11:51 -0800

    a very big sum solution

---
+++ Problem Solving/Algorithms/aVeryBigSum.js
@@ -0,0 +1,12 @@
+function aVeryBigSum(ar) {
+  // Write your code here
+  var arrSum = 0;
+  var arrLeng = ar.length;
+
+  for (var i = 0; i <= arrLeng -1; i++) {
+    arrSum += ar[i];
+  }
+  
+  console.log(arrSum);
+  return arrSum;
+}
