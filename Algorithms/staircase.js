ab2fcaecffe21bd4785aa13fd50a8e04c776a511
Collin Dapper <collindapper@gmail.com>
2022-12-23 11:59:47 -0800

    Staircase Solution

--- Algorithms/staircase.js
+++ Algorithms/staircase.js
@@ -1,3 +1,12 @@
 function staircase(n) {
-
+  var total = n;
+  
+  for (var i = 1; i <= n; i++) {
+    if (total > 0) {
+      total -= 1;
+    } else {
+      total = 0;
+    }
+    console.log(" ".repeat(total) + "#".repeat(i));
+  }
 }
